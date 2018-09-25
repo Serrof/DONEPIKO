f10928d33095f68ea2f41abc25ed61847e2cf33c
Serrof <Serrof@users.noreply.github.com>
2018-09-25 21:51:24 +0200

    Add files via upload

---
+++ script_compa_approach.py
@@ -0,0 +1,56 @@
+# script_compa_approach.py: script comparing the results of the direct and indirect approaches 
+# Copyright(C) 2018 Romain Serra
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Software Foundation, either version 3 of the License, or any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this program.
+# If not, see < https://www.gnu.org/licenses/>.
+
+import utils
+import random
+import numpy
+import math
+import master
+import plotter
+import matplotlib.pyplot as plt
+import dynamics_factory
+import time
+
+nu0 = 2.0 * math.pi * random.random()  # initial true anomaly in rad
+nuf = nu0 + 2.0 * math.pi * random.random()  # final true anomaly in rad
+
+dyn = dynamics_factory.EarthMoonLP(Li=4)  # 3-body restricted dynamics is linearized around Earth-Moon Lagrange Point 4
+
+hd = 3  # half-dimension of state vector (1 for out-of-plane dynamics only, 2 for in-plane only and 3 for complete)
+x0 = numpy.zeros(hd * 2)
+xf = numpy.zeros(hd * 2)
+for i in range(0, len(x0)):
+	if i < hd:
+		x0[i] = (-1.0 + 2.0 * random.random()) * 1.0e3
+		xf[i] = (-1.0 + 2.0 * random.random()) * 1.0e3
+	else:
+		x0[i] = (-1.0 + 2.0 * random.random()) * 1.0e3 * math.pi / dyn.period
+		xf[i] = (-1.0 + 2.0 * random.random()) * 1.0e3 * math.pi / dyn.period
+
+p = 2  # norm for minimization (1 or 2)
+
+BC = utils.BoundaryConditions(nu0, nuf, x0, xf)
+plr = plotter.Plotter(dyn, BC, p, anomaly=True, linearized=True)
+master = master.Master(indirect=True, p=p, plr=plr)
+
+t1 = time.time()
+master.solve()
+print("Time elapsed with indirect approach: " + str(time.time() - t1) + " s")
+master.plot()
+master.write_boundary_cond("boundary_conditions_compa_approach.txt")
+master.write_control_law("control_law_indirect.txt")
+
+master.set_approach(indirect=False)
+t2 = time.time()
+master.solve()
+print("Time elapsed with direct approach: " + str(time.time() - t2) + " s")
+master.plot()
+master.write_control_law("control_law_direct.txt")
+
+plt.show()
---
+++ script_compa_suboptim.py
@@ -0,0 +1,46 @@
+# script_compa_suboptim.py: script showing that the two-impulse strategy with boundary burns is usually not optimal
+# Copyright(C) 2018 Romain Serra
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Software Foundation, either version 3 of the License, or any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this program.
+# If not, see < https://www.gnu.org/licenses/>.
+
+import utils
+import random
+import numpy
+import math
+import master
+import plotter
+import matplotlib.pyplot as plt
+import dynamics_factory
+
+nu0 = 2.0 * math.pi * random.random()  # initial true anomaly in rad
+nuf = nu0 + 2.0 * math.pi * random.random()  # final true anomaly in rad
+
+dyn = dynamics_factory.GEO()  # 2-body restricted dynamics is linearized around Geostationary Earth Orbit
+
+hd = 2  # half-dimension of state vector (1 for out-of-plane dynamics only, 2 for in-plane only and 3 for complete)
+x0 = numpy.zeros(hd * 2)
+xf = numpy.zeros(hd * 2)
+for i in range(0, len(x0)):
+	if i < hd:
+		x0[i] = (-1.0 + 2.0 * random.random()) * 1.0e3
+		xf[i] = (-1.0 + 2.0 * random.random()) * 1.0e3
+	else:
+		x0[i] = (-1.0 + 2.0 * random.random()) * 1.0e3 * math.pi / dyn.period
+		xf[i] = (-1.0 + 2.0 * random.random()) * 1.0e3 * math.pi / dyn.period
+
+p = 1  # norm for minimization (1 or 2)
+
+BC = utils.BoundaryConditions(nu0, nuf, x0, xf)
+plr = plotter.Plotter(dyn, BC, p, anomaly=True, linearized=True)
+master = master.Master(indirect=True, p=p, plr=plr)
+master.write_boundary_cond("boundary_conditions_suboptim.txt")
+
+master.suboptimize()
+master.write_control_law("control_law_suboptim.txt")
+master.plot()
+
+plt.show()
